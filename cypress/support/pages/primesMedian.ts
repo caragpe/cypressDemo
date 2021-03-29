7af6a3276867cd9d299fe99f43b3d26a5481e4f1
Carlos Agullo <software.development.2@olympicchannel.com>
2021-03-30 00:24:26 +0200

    moving all cypress files to ts

---
+++ cypress/support/pages/primesMedian.ts
@@ -0,0 +1,6 @@
+export const container = '#mainContainer';
+export const containerHeader = 'h1';
+export const inputField = 'form input';
+export const submitButton = 'form button';
+export const waitingToCalculateSpinner = '.fa-spinner';
+export const resultMessage = 'h2';
